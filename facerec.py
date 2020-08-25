c7e9b0f6b22e3dae023cebbe1dee7dfe19be4b87
Mukund Shukla <mukundshukla000@gmail.com>
2020-08-25 10:01:05 +0530

    face recognition project /opencv

---
+++ facerec.py
@@ -0,0 +1,59 @@
+import face_recognition
+import cv2
+import numpy as np
+
+cam = cv2.VideoCapture(0)
+
+mukund_image =  face_recognition.load_image_file("me.jpg")
+mukund_face_encoding = face_recognition.face_encodings(mukund_image)[0]
+
+baba_image = face_recognition.load_image_file("baba.jpg")
+baba_face_encoding = face_recognition.face_encodings(baba_image)[0]
+
+known_face_encodings = [mukund_face_encoding,baba_face_encoding,radha_face_encoding,dev_face_encoding]
+known_face_names = ["Mukund","Baba"]
+
+while True:
+    #taking picture
+    ret, frame = cam.read()
+
+    #converting BGR to RGB
+    rgb_frame = frame[:, :, ::-1]
+
+    face_locations = face_recognition.face_locations(rgb_frame)
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+
+        name = "Unknown"
+
+        # If a match was found in known_face_encodings, just use the first one.
+        # if True in matches:
+        #     first_match_index = matches.index(True)
+        #     name = known_face_names[first_match_index]
+
+        # Or instead, use the known face with the smallest distance to the new face
+        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
